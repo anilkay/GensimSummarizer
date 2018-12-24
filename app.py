bb2a3bd066da5d0ed002ef6f1515b5b049365ee1
Anıl Kaynar <aanilkay@gmail.com>
2018-12-24 13:13:48 +0300

    heroku interestings

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_cors import CORS, cross_origin
 
 from flask import request
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/')
 def hello_world():
     return "HELLO WORLD"
